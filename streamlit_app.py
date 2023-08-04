448aa90a8526940947550882ff7329409e938aa7
Jules Kuehn <jk@jules.lol>
2023-08-04 13:11:25 -0400

    blah

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,30 @@
+import tempfile
 import streamlit as st
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
+from langchain.document_loaders import PyPDFLoader
 
 from vertex_ai.vertex_ai import gcp_embeddings, gcp_llm, gcp_chat
+
 
 def generate_response(uploaded_file, query_text):
     # Load document if file is uploaded
     if uploaded_file is not None:
-        documents = [uploaded_file.read().decode()]
+        # Check if document is PDF
+        if uploaded_file.type == "application/pdf":
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as tmp:
+                tmp.write(uploaded_file.getvalue())
+                tmp_location = tmp.name
+            
+            loader = PyPDFLoader(tmp_location)
+            pages = loader.load_and_split()
+            documents = [page.text for page in pages]
+        # Otherwise, read as text
+        else:
+            documents = [uploaded_file.read().decode()]
         # Split documents into chunks
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
         texts = text_splitter.create_documents(documents)
         # Select embeddings
         embeddings = gcp_embeddings
@@ -19,25 +33,36 @@ def generate_response(uploaded_file, query_text):
         # Create retriever interface
         retriever = db.as_retriever()
         # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=gcp_chat, chain_type='stuff', retriever=retriever)
+        qa = RetrievalQA.from_chain_type(
+            llm=gcp_chat, chain_type="stuff", retriever=retriever
+        )
         return qa.run(query_text)
 
 
 # Page title
-st.set_page_config(page_title='🦜🔗 Ask the Doc App')
-st.title('🦜🔗 Ask the Doc App')
+st.set_page_config(page_title="🦜🔗 Ask the Doc App")
+st.title("🦜🔗 Ask the Doc App")
 
 # File upload
-uploaded_file = st.file_uploader('Upload an article', type='txt')
+uploaded_file = st.file_uploader(
+    "Upload a document",
+    type=["txt", "pdf", "html", "md", "csv", "json", "xml"],
+)
 # Query text
-query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
+query_text = st.text_input(
+    "Enter your question:",
+    placeholder="Please provide a short summary.",
+    disabled=not uploaded_file,
+)
 
 # Form input and query
 result = []
-with st.form('myform', clear_on_submit=True):
-    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
+with st.form("myform", clear_on_submit=True):
+    submitted = st.form_submit_button(
+        "Submit", disabled=not (uploaded_file and query_text)
+    )
     if submitted:
-        with st.spinner('Calculating...'):
+        with st.spinner("Calculating..."):
             response = generate_response(uploaded_file, query_text)
             result.append(response)
 
